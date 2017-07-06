f3975be0905103fff4e412753b8087bcb1008261
George Nychis <george@softwareroboticscorp.com>
2017-07-05 23:00:22 -0400

    going to convert the tweets to base64 now before processing

--- boiserver/check_for_processing.py
+++ boiserver/check_for_processing.py
@@ -1,10 +1,26 @@
 from time import sleep
 from datetime import datetime
 from .database import db_connect, tweet_table_session, Tweet
+import base64
+from typing import Dict, Optional
+
+import json
 
 from . import ELAPSED_SECONDS_UNTIL_QUEUE
 
-from sqlalchemy.sql import select
+
+class TweetForProcessing:
+
+    def __init__(self, tweet_id: str=Optional[None], image: str=Optional[None]):
+
+        self.tweet_id = tweet_id
+        self.image = image
+
+    def dict(self) -> Dict:
+        return {
+            'tweet_id': self.tweet_id,
+            'image': self.image
+        }
 
 
 def check_for_processing() -> None:
@@ -22,6 +38,14 @@ def check_for_processing() -> None:
             if r.queued_at is None or (datetime.now()-r.queued_at).total_seconds() > ELAPSED_SECONDS_UNTIL_QUEUE:
                 print(r.tweet_id, r.queued_at)
                 r.queued_at = datetime.now()
+
+                # Base64 the image and get ready to send it.
+                b64_image = base64.b64encode(r.image)
+
+                # Convert it to a jsonable form
+                tfp = TweetForProcessing(r.tweet_id, b64_image)
+                payload = json.dumps(tfp.dict())
+
 
                 session.add(r)
                 session.flush()
