4343399201aaeef1eb45b23a1f51a1deccdfe895
George Nychis <george@softwareroboticscorp.com>
2017-07-08 11:10:52 -0400

    adding more items, now printing out the proper name

--- boiserver/tweet_reply.py
+++ boiserver/tweet_reply.py
@@ -1,7 +1,21 @@
 from . import reply_queue
+from os import path
+import xml.etree.ElementTree as ET
+import json
 
 
 def check_for_tweet_reply() -> None:
+
+    # Read through the items.xml that is extracted from the game to get a mapping of sprite names to human
+    # readable names.
+    tree = ET.parse('boiserver/items.xml')
+    root = tree.getroot()
+
+    items = {}
+    for child in root:
+        items[child.attrib["gfx"].lower()] = child.attrib
+
+    print(items)
 
     while True:
 
@@ -11,8 +25,14 @@ def check_for_tweet_reply() -> None:
 
         # Split up the tweet ID and the responses.
         tweet_id = reply_to_parse.split("|")[0]
+
+        # Get the found images out, dealing with formatting (get rid of base path)
         found_images = reply_to_parse.split("|")[1].split(";")
         found_images = list(filter(None, found_images))
+        found_images = [path.basename(element) for element in found_images]
 
+        print("----------------------")
         print(tweet_id)
-        print(found_images)
+
+        for img in found_images:
+            print(items[img]["name"])
