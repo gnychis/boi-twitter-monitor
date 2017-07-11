cbc57c7de401111caf659dd061fbde7d49d6c899
George Nychis <george@softwareroboticscorp.com>
2017-07-10 21:41:12 -0400

    fixing a few links

--- boiserver/tweet_reply.py
+++ boiserver/tweet_reply.py
@@ -72,11 +72,8 @@ def check_for_tweet_reply() -> None:
             twitter.update_status(status=message, in_reply_to_status_id=tweet_id, media_ids=image_ids['media_id'])
 
         if len(result["matches"]) == 0:
-            message = """Oh no! I couldn't find anything.  Paging @gnychis for help."""
-            try:
-                twitter.update_status(status=message, in_reply_to_status_id=tweet_id)
-            except:
-                pass
+            message = """@{} Oh no! I couldn't find anything.  Paging @gnychis for help.""".format(tweet_entry.author)
+            twitter.update_status(status=message, in_reply_to_status_id=tweet_id)
 
         # Save the entry
         session.add(tweet_entry)
